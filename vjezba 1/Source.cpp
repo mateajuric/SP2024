c481c6c3ef6d52a4bbebb7ffd3d97602e5808d69
„ViceElez” <vice.elezovic12@gmail.com>
2024-10-07 19:49:55 +0200

    change

--- vjezba 1/Source.cpp
+++ vjezba 1/Source.cpp
@@ -1,6 +1,33 @@
 #include <stdio.h>
+#include <stdlib.h>
+#define _CRT_SECURE_NO_WARNINGS
+#define ERROR_OP 1
+#define MAX 1024
+
+int brojredaka(const char*);
+
 int main()
 {
-	printf("Hello world!");
+	int brredaka=0;
+	brredaka = brojredaka("imedatoteke.txt");
 	return 0;
+}
+int brojredaka(const char*fileName)
+{
+	FILE *fp = NULL;
+	char buffer[MAX] = {};
+	int broj = 0;
+	fp = fopen(fileName,"r");
+	if (fp==NULL)
+	{
+		printf("Greska pri otvaranju datoteke.");
+		return ERROR_OP;
+	}
+	while (!feof(fp))
+	{
+		fgets(buffer, MAX, fp);
+		broj++;
+	}
+	fclose(fp);
+
 }
